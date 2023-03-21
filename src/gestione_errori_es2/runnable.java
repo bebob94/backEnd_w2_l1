d9a52691928973854077a46e6ec26f5741f49b29
bebob94 <bebo.macis@gmail.com>
2023-03-21 09:38:01 +0100

    add

--- src/gestione_errori_es2/runnable.java
+++ src/gestione_errori_es2/runnable.java
@@ -3,25 +3,36 @@ package gestione_errori_es2;
 import java.util.Scanner;
 
 public class runnable {
+	private static Scanner input = new Scanner(System.in);
+	private static double km, litri, kmPerLitro;
 
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        double km, litri, kmPerLitro;
-
-        System.out.println("Inserisci il numero di km percorsi: ");
-        km = input.nextDouble();
-        System.out.println("Inserisci il numero di litri di carburante consumati: ");
-        litri = input.nextDouble();
-
+    	input();
+    	
         try {
-            if (litri == 0) {
-                throw new Exception("Divisione per zero: il numero di litri non può essere uguale a 0!");
-            } else {
-                kmPerLitro = km / litri;
-                System.out.printf("Il numero di km/litro percorsi è: "+  kmPerLitro + " km/litro");
-            }
+           calcola();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    
+    
+//<<<<<<<<<<<<<<<<<<<<<<METODO INPUT>>>>>>>>>>>>>>>>>>>>>>
+    public static void input() {
+    	 System.out.println("Inserisci il numero di km percorsi: ");
+         km = input.nextDouble();
+         System.out.println("Inserisci il numero di litri di carburante consumati: ");
+         litri = input.nextDouble();
+    }
+    
+//<<<<<<<<<<<<<<<<<<<<<METODO CALCOLA>>>>>>>>>>>>>>>>>>>>>    
+    public static void calcola() throws Exception {
+    	 if (litri == 0) {
+             throw new Exception("Divisione per zero: il numero di litri non può essere uguale a 0!");
+         } else {
+             kmPerLitro = km / litri;
+             System.out.printf("Il numero di km/litro percorsi è: "+  kmPerLitro + " km/litro");
+         }
+    }
+    
 }
